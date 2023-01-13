0299a02416dc895ef41f8b24e493a6dc2ee95774
Sam Kim <sam.kim@mirpartners.com>
2023-01-13 13:11:34 -0500

    Add more base components

---
+++ src/components/brewery-list-containter-view/brewery-list-container-view.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@chakra-ui/react"
+import { FC } from "react"
+import BreweryListView from "../brewery-list-view/brewery-list-view"
+import SearchView from "../search-view/search-view"
+import { BreweryListContainerViewProps } from "./brewery-list-container-view.props"
+
+const BreweryListContainerView: FC<BreweryListContainerViewProps> = () => {
+  return (
+    <Flex direction="column" background="pink">
+      <h1>BreweryListContainerView</h1>
+      <SearchView />
+      <BreweryListView />
+    </Flex>
+  )
+}
+
+export default BreweryListContainerView
--- src/components/map-view/map-view.tsx
+++ src/components/map-view/map-view.tsx
@@ -1,8 +1,59 @@
-import { FC } from "react"
+import { Flex, Spinner } from "@chakra-ui/react"
+import { FC, useCallback, useRef, useState } from "react"
+import useToggle from "../../hooks/useToggle"
 import { MapViewProps } from "./map-view.props"
 
-const MapView: FC<MapViewProps> = () => {
-  return <h1>MapView</h1>
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+  LoadScript,
+  useJsApiLoader,
+} from "@react-google-maps/api"
+import mapStyles from "../../../lib/mapStyles"
+
+const options = {
+  // styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+}
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "100vh",
+}
+
+const MapView: FC<MapViewProps> = (
+  {
+    // breweries,
+    // userLocation,
+    // display,
+    // displayList,
+    // mapWidth,
+    // mapHeight,
+    // mapZoom,
+    // mapCenter,
+  }
+) => {
+  // const [selectedBrew, setSelectedBrew] = useState(null)
+  const [markerView, toggleMarkerView] = useToggle(true)
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+  })
+
+  console.log("isLoaded", isLoaded)
+
+  if (!isLoaded) return <Spinner />
+  return (
+    <Flex>
+      <GoogleMap
+        zoom={10}
+        center={{ lat: 44, lng: -80 }}
+        mapContainerStyle={mapContainerStyle}
+      ></GoogleMap>
+    </Flex>
+  )
 }
 
 export default MapView
