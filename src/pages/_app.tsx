eaf787a7762e1b2c4d35e5621a7c22699b828e89
Sam Kim <sam.kim@mirpartners.com>
2023-02-15 19:32:24 -0500

    Intgrate supabase auth components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-// import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
-// import { SessionContextProvider, Session } from "@supabase/auth-helpers-react"
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react"
 import { useState } from "react"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
@@ -19,21 +19,22 @@ const theme = extendTheme({ colors })
 
 // TODO: Set up Auth Supabase
 
-export function MyApp(
-  { Component, pageProps }: AppProps /* <{
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
   initialSession: Session
-}> */
-) {
-  // const [supabase] = useState(() => createBrowserSupabaseClient())
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
   return (
-    // <SessionContextProvider
-    //   supabaseClient={supabase}
-    //   initialSession={pageProps.initialSession}
-    // >
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-    // </SessionContextProvider>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionContextProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import { Heading } from "@chakra-ui/react"
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react"
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react"
+import Account from "../components/account/account"
 
-export default function Home() {
-  // TODO: Supbase Auth
-  // const session = useSession()
-  // const supabase = useSupabaseClient()
+const Home = () => {
+  const session = useSession()
+  const supabase = useSupabaseClient()
+
   return (
-    <>
-      <Head>
-        <title>Brewery Finder App</title>
-        <meta name="description" content="Brewery Finder App" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <Heading>Welcome to the Brewery Finder</Heading>
-      </main>
-    </>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+        />
+      ) : (
+        <Account session={session} />
+      )}
+    </div>
   )
 }
+
+export default Home
