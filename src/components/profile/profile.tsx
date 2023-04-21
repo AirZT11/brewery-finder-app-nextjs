2940cc6ab61d14458d8f86f4b738293e172e7a84
Sam Kim <samjkim11@gmail.com>
2023-04-21 15:56:01 -0400

    Profile page should animate in

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,21 +1,11 @@
-import {
-  Box,
-  Container,
-  Flex,
-  Heading,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-} from "@chakra-ui/react"
+import { Flex, Heading } from "@chakra-ui/react"
 import { FC } from "react"
 import { useGetProfileQuery } from "../../store/features/api/profilesApiSlice"
 import { useGetRatingsByUserIdQuery } from "../../store/features/api/ratingsApiSlice"
 import UserAvatar from "../user-avatar/user-avatar"
 import ReviewListView from "../review-list-view/review-list-view"
 import { ProfileProps } from "./profile.props"
+import { motion } from "framer-motion"
 
 const Profile: FC<ProfileProps> = ({ username }) => {
   const { data: user } = useGetProfileQuery(username)
@@ -26,28 +16,36 @@ const Profile: FC<ProfileProps> = ({ username }) => {
   })
 
   return (
-    // <Container p="8" h="full">
-    <Flex direction="column" align="center" p="8" h="100%" overflow="hidden">
-      {user?.avatar_url && (
-        <UserAvatar uid={user?.id!} url={user.avatar_url} size={150} />
-      )}
-      <Heading my="4">{username}</Heading>
-
-      {/* <Text>Reviews:</Text> */}
-      <Flex
-        overflow="auto"
-        borderColor="background.300"
-        borderWidth="2px"
-        bg="background.100"
-        p="4"
-        borderRadius="8"
-      >
-        {breweryRatings && (
-          <ReviewListView breweryRatings={breweryRatings} isProfileView />
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      }}
+      exit={{ opacity: 0, scale: 0.5 }}
+    >
+      <Flex direction="column" align="center" p="8" h="100%" overflow="hidden">
+        {user?.avatar_url && (
+          <UserAvatar uid={user?.id!} url={user.avatar_url} size={150} />
         )}
+        <Heading my="4">{username}</Heading>
+
+        <Flex
+          overflow="auto"
+          borderColor="background.300"
+          borderWidth="2px"
+          bg="background.100"
+          p="4"
+          borderRadius="8"
+        >
+          {breweryRatings && (
+            <ReviewListView breweryRatings={breweryRatings} isProfileView />
+          )}
+        </Flex>
       </Flex>
-    </Flex>
-    // </Container>
+    </motion.div>
   )
 }
 
